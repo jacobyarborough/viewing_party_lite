25ad4453d90a9bffd6ebabd7b346b01606b8263d
Jacob Yarborough <jayarborough95@gmail.com>
2021-12-15 12:02:42 -0700

    Add password_digest to user model and update testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :invites
   has_many :parties, dependent: :destroy
+  has_secure_password 
 
-  validates_presence_of :name, :email, uniqueness: true
+  validates_presence_of :password_digest, :name, :email, uniqueness: true
 
   def invitations
     invites.joins(:party).select('invites.user_id as invitee_id, parties.*')
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_204101) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
